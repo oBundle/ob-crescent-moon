fed688f7e5c0cc94818dec95df82a1c5905d8031
mgrienauer <mgrienauer@gmail.com>
2020-09-04 10:38:15 -0700

    Update homepage animation

--- assets/js/theme/ob-custom/hero-animation.js
+++ assets/js/theme/ob-custom/hero-animation.js
@@ -2,11 +2,18 @@ import anime from 'animejs/lib/anime.es.js';
 
 export default function() {
 
-  
+  $('.ob-hero-img-white-overlay').fadeOut("slow")
 
   anime({
+    targets: '.ob-hero-img-bg',
+    translateX: 25,
+    easing: 'linear',
+    duration: 10000
+  })
+  
+  anime({
     targets: '.ob-hero-img-1',
-    translateX: -100,
+    translateX: -50,
     easing: 'linear',
     duration: 10000
   })
@@ -20,20 +27,24 @@ export default function() {
 
   anime({
     targets: '.ob-hero-img-3',
-    translateX: 100,
+    translateX: -100,
     easing: 'linear',
     duration: 10000
   })
 
   anime({
     targets: '.ob-hero-img-4',
-    translateX: -100,
+    translateX: 100,
     easing: 'linear',
     duration: 10000
   })
 
-  setTimeout(() => $('#ob-hero-heading').fadeIn("slow"), 1000)
-  setTimeout(() => $('#ob-hero-ctas').fadeIn("slow"), 1500)
+  
+  setTimeout(() => $('#ob-hero-heading').fadeIn(1500), 1500)
+  setTimeout(() => $('#ob-hero-ctas').fadeIn(1500), 2500)
+  setTimeout(() => $('#hero-cta-btn').fadeIn(1500), 2500)
+  setTimeout(() => $('#hero-cta-link').fadeIn(2000), 5000)
+
 
 }
 
