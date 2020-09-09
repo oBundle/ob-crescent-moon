7a9e0900aa3bd6f00f863e8e236ebe57f34412dd
mgrienauer <mgrienauer@gmail.com>
2020-09-09 13:56:08 -0700

    add conditional render logic to subnav

--- assets/js/theme/ob-custom/cat-subnav.js
+++ assets/js/theme/ob-custom/cat-subnav.js
@@ -1,3 +1,28 @@
 export default function(context) {
-  console.log('subnav ctx', context)
+  // target ob cat subnav links
+  const $subnavLinks = $(".ob-cat-subnav-link")
+  //target product grid
+  let $productGrid = $("li.product")
+
+  //subnav link click handler
+  $subnavLinks.on("click", (event) => {
+    event.preventDefault()
+    //target subnavlink title (which is the cat or subcat)
+    const subnavCat = $(event.currentTarget).attr('title');
+
+    //loop over product grid
+    $productGrid.each((index, product) => {
+      //target data-product-cats (string with all cats / subcats this product is in)
+      const categories = $(product).attr('data-product-cats')
+      //if subnav link's title is not in data-product/cats as substr
+      if (!categories.includes(subnavCat)) {
+        //hide the product card
+        $(product).hide()
+      //else, show this product card
+      } else {
+        $(product).fadeIn()
+      }
+    })
+
+  })
 }
