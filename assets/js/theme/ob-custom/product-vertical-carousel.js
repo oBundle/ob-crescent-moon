83dc69d2dac82e1eda3755a3153bbcc708dc9bfa
mgrienauer <mgrienauer@gmail.com>
2020-09-17 11:50:43 -0700

    Finish product page mobile styles

--- assets/js/theme/ob-custom/product-vertical-carousel.js
+++ assets/js/theme/ob-custom/product-vertical-carousel.js
@@ -1,4 +1,23 @@
 export default function() {
+
+  $('.productView-thumbnails').slick({
+    infinite: true,
+    vertical: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    dots: false,
+    arrows: true,
+    verticalSwiping: true,
+  });
+
+  $('.productView-thumbnails-mobile').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: false,
+    arrows: true,
+  });
+        
   $('.ob-carousel-vertical-prev').on('click', () => {
     const $slickPrev = $('.ob-carousel-vertical-prev').parent().find('.slick-prev')
     return $slickPrev.click()
